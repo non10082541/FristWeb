44384cefefea4fbd19b13dc7c60136f651b7e0b9
Nontawat <nontawat555963@gmail.com>
2022-02-26 22:16:01 +0700

    add feed profile activities
    เพิ่มหน้าโปรไฟล์ ฟีดที่มีการแสดงความคิดเห็น เพิ่มประวัติการแสดงความคิดเห็น แก้บัคอีกเล็กน้อย

--- base/views.py
+++ base/views.py
@@ -76,8 +76,10 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=s))
 
-    context =  {'room': rooms, 'topics': topics, 'room_count': room_count}
+
+    context =  {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
@@ -98,6 +100,15 @@ def room(request, pk):
                 'participants': participants,}
     return render(request, 'base/room.html', context)
 
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+
+    context = {'user': user, 'rooms': rooms, 'room_messages': room_messages, 'topics': topics}
+    return render(request, 'base/profile.html', context)
+
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
@@ -105,7 +116,9 @@ def createRoom(request):
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect('home')
 
     context = {'form': form}
